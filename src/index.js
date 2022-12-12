28b27d37f4af95b75dde636a3fe9862a69680b9d
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-13 00:55:10 +0300

    src/index.js: add greeting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('start');
+import process from 'node:process';
+import { parseProcessArgs } from './utils/index.js';
+
+const args = parseProcessArgs();
+
+const handleProcessExit = () => {
+  console.log(`Thank you for using File Manager, ${args.username}, goodbye!`);
+  process.exit(0);
+};
+
+process.stdin.on('data', (data) => {
+  const command = data.toString().trim();
+
+  if (command === '.exit') {
+    handleProcessExit();
+  }
+});
+
+process.on('SIGINT', handleProcessExit);
+  
+console.log(`Welcome to the File Manager, ${args.username}!`);
