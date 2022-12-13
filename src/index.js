df1132b26cf8c29fb91cb67329ab9a947e33515f
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-13 23:54:58 +0300

    src/index.js: add commaneds handlers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import process from 'node:process';
 import { parseProcessArgs } from './utils/index.js';
 
+const getCommandFromData = (data) => {
+  const row = data.split(' ');
+
+  return {
+    command: row[0],
+    args: row[1],
+  };
+};
+
+const showCurrentDirName = () => {
+  console.log(`You are currently in ${process.cwd()}`);
+};
+
 const args = parseProcessArgs();
 
 const handleProcessExit = () => {
@@ -8,14 +21,44 @@ const handleProcessExit = () => {
   process.exit(0);
 };
 
+const handleProcessUpInput = () => {
+  process.chdir('../');
+};
+
+const handleProcessCdInput = (path) => {
+  if (!path) {
+    return;
+  }
+
+  process.chdir(path);
+};
+
 process.stdin.on('data', (data) => {
-  const command = data.toString().trim();
+  const {
+    command,
+    args,
+  } = getCommandFromData(data.toString().trim());
+
+  if (!command) {
+    return;
+  }
 
   if (command === '.exit') {
     handleProcessExit();
   }
+
+  if (command === 'up') {
+    handleProcessUpInput();
+  }
+
+  if (command === 'cd') {
+    handleProcessCdInput(args);
+  }
+
+  showCurrentDirName();
 });
 
 process.on('SIGINT', handleProcessExit);
   
 console.log(`Welcome to the File Manager, ${args.username}!`);
+showCurrentDirName();
