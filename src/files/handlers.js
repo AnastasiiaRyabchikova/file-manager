862a9757bf13a7e6045cc1fcefa31f47e52137c6
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-18 18:17:57 +0300

    src/files/handlers.js: add handlers

--- src/files/handlers.js
+++ src/files/handlers.js
@@ -1,4 +1,13 @@
-import { createFile, renameFile } from "./utils.js";
+import {
+  createFile,
+  renameFile,
+  removeFile,
+  readFile,
+} from "./utils.js";
+
+const handleProcessCatInput = ([path]) => {
+  readFile(path);
+};
 
 const handleProcessAddInput = ([path]) => {
   createFile(path);
@@ -8,7 +17,24 @@ const handleProcessRnInput = (args) => {
   renameFile(args[0], args[1]);
 };
 
+const handleProcessCpInput = ([newPath, oldPath]) => {
+  console.log('cp', { newPath, oldPath });
+};
+
+const handleProcessMvInput = ([ newPath, oldPath ]) => {
+  console.log('mv', { newPath, oldPath });
+};
+
+const handleProcessRmInput = ([ path ]) => {
+  removeFile(path);
+};
+
+
 export const handlers = {
+  cat: handleProcessCatInput,
   add: handleProcessAddInput,
-  rn: handleProcessRnInput
+  rn: handleProcessRnInput,
+  cp: handleProcessCpInput,
+  mv: handleProcessMvInput,
+  rm: handleProcessRmInput,
 };
