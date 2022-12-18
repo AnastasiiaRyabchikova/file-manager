74949c37ce9cf40991a42d0eab5fdf102c3295af
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-18 17:22:02 +0300

    src/files/utils.js: add rename file

--- src/files/utils.js
+++ src/files/utils.js
@@ -1,8 +1,16 @@
-import { appendFile } from 'node:fs/promises';
+import { appendFile, rename } from 'node:fs/promises';
 
 export const createFile = async (path) => {
   try {
     await appendFile(path, '', { flaf: 'ax' });
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+export const renameFile = async (oldPath, newPath) => {
+  try {
+    await rename(oldPath, newPath);
   } catch (error) {
     console.error(error)
   }
